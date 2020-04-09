056e6957f35f18c308813a7d5a76f059bedbfab4
aolin <aolin@315net.com>
2020-04-09 23:44:28 +0800

    2020.4.9修改2

---
+++ cloud-eureka-server7001/src/test/java/aoao/Tests.java
@@ -0,0 +1,15 @@
+package aoao;
+
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+    @Test
+    public void say() {
+        System.err.println("王夫子");
+    }
+
+    @Test
+    public void sayHe() {
+
+    }
+}
